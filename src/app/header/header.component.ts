66af67919613592a456a4cb74aa3675ef8483212
AmpatzidisSavvas <savvas.ampa@gmail.com>
2024-03-04 11:38:59 +0200

    Routing complete

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,9 +7,4 @@ import { Component, EventEmitter, Output } from '@angular/core';
 })
 export class HeaderComponent { 
 
-  @Output() feactureSelected = new EventEmitter<string>();
-
-  onSelect(feature: string) { 
-    this.feactureSelected.emit(feature);
-  }
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -32,6 +32,10 @@ export class RecipeService {
     return this.recipes.slice();
   }
 
+  getRecipe(index: number) {
+    return this.recipes[index];
+  }
+
   onAddIngredientsToShoppingList(ingredients: Ingredient[]) {
     this.shoppingListService.addIngredients(ingredients);
   }
