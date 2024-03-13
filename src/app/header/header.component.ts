2bb7ab9b9262c09b77b03ba2b2b3585d883742cf
AmpatzidisSavvas <savvas.ampa@gmail.com>
2024-03-13 11:03:46 +0200

    Authentication complete

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,14 +1,25 @@
-import { Component} from '@angular/core';
+import { Component, OnDestroy, OnInit} from '@angular/core';
 import { DataStorageService } from '../shared/data-storage.service';
+import { AuthService } from '../auth/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
 
-  constructor(private serviceDataStorage: DataStorageService) {}
+  private userSub: Subscription;
+  isAuthenticated = false;
+
+  constructor(private serviceDataStorage: DataStorageService, private authService: AuthService) {}
+
+  ngOnInit() {
+    this.userSub= this.authService.user.subscribe(user => {
+      this.isAuthenticated = !user ? false : true;
+    });
+  }
 
   onSaveData() {
     this.serviceDataStorage.storeRecipes();
@@ -16,6 +27,14 @@ export class HeaderComponent {
 
   onFetchData() {
     this.serviceDataStorage.fetchRecipes().subscribe();
+  }
+
+  onLogout() {
+    this.authService.logout();
+  }
+
+  ngOnDestroy(): void {
+    this.userSub.unsubscribe();
   }
 
 }
