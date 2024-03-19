1580dc99ae26468228395c413c32b8c5b76f3b83
AmpatzidisSavvas <savvas.ampa@gmail.com>
2024-03-19 11:54:37 +0200

    Enviroments Added

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    apiKey: 'AIzaSyDOsShASixJBnooYB63NWy5gbDjyi5mBmo',
+  };
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    apiKey: 'AIzaSyDOsShASixJBnooYB63NWy5gbDjyi5mBmo',
+  };
